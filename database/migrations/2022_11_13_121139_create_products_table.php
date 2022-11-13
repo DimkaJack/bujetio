06a2752bb696b2da0b4627d36e65d8ab4781b1aa
D.Vorozhtsov <vorozhtsov@self.team>
2022-11-13 18:05:33 +0400

    Добавил sail, добавил структуру бд

---
+++ database/migrations/2022_11_13_121139_create_products_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('name');
+            $table->string('type');
+            //@todo extract balance to entity
+            $table->integer('start_balance_amount');
+            $table->string('start_balance_currency');
+            $table->integer('balance_amount');
+            $table->string('balance_currency');
+            $table->timestamps();
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('transactions');
+    }
+};
---
+++ database/migrations/2022_11_13_125811_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->string('type');
+            $table->foreignUuid('product_id')->constrained('products');
+            $table->foreignUuid('category_id')->constrained('categories');
+            $table->timestamps();
+
+            $table->primary('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropForeign(['product_id', 'category_id']);
+        });
+        Schema::dropIfExists('transactions');
+    }
+};
