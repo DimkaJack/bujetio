c62dd5316dbbbd9eb2c8bc293604732e5a723059
D.Vorozhtsov <d.vorozhcov@mastercr.ru>
2023-12-28 05:03:34 +0400

    add money formatter

---
+++ resources/js/Components/MoneyFormatter.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { computed } from "vue";
+
+const props = defineProps(['value', 'currency']);
+const formattedValue = computed(() => {
+    let formatter = new Intl.NumberFormat('ru-RU', {
+        style: 'currency',
+        currency: props.currency,
+        currencyDisplay: 'narrowSymbol'
+    });
+    return formatter.format(props.value);
+});
+</script>
+
+<template>
+    <span>{{ formattedValue }}</span>
+</template>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import {Head} from '@inertiajs/inertia-vue3';
+import MoneyFormatter from "@/Components/MoneyFormatter.vue";
 
 defineProps(['products', 'transactions', 'budgets']);
 </script>
@@ -15,13 +16,17 @@ defineProps(['products', 'transactions', 'budgets']);
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white shadow-sm sm:rounded-lg grid grid-cols-3">
+                <div class="bg-white shadow-sm sm:rounded-lg grid grid-cols-2">
                     <div class="p-6 text-gray-900">
                         <h3 class="text-lg font-semibold pb-4">Last transactions:</h3>
                         <div>
                             <div v-for="transaction in transactions.data">
                                 <span class="text-xs">{{ transaction.payDate }}</span>
-                                {{ transaction.name }}. {{ transaction.amount}} {{ transaction.amountCurrency}}
+                                {{ transaction.name }}.
+                                <MoneyFormatter
+                                    :value="transaction.amount"
+                                    :currency="transaction.amountCurrency"
+                                />
                             </div>
                         </div>
                     </div>
@@ -29,7 +34,11 @@ defineProps(['products', 'transactions', 'budgets']);
                         <h3 class="text-lg font-semibold pb-4">Products:</h3>
                         <div>
                             <div v-for="product in products.data">
-                                {{ product.name }}. {{ product.balanceAmount}} {{ product.balanceCurrency}}
+                                {{ product.name }}.
+                                <MoneyFormatter
+                                    :value="product.balanceAmount"
+                                    :currency="product.balanceCurrency"
+                                />
                             </div>
                         </div>
                     </div>
@@ -37,18 +46,48 @@ defineProps(['products', 'transactions', 'budgets']);
                         <span>WIP</span>
                         <h3 class="text-lg font-semibold pb-4">Budget(month): </h3>
                         <ul>
-                            <li>income: {{ budgets.monthly.income.amount }} {{ budgets.monthly.income.currency }}</li>
-                            <li>outcome: {{ budgets.monthly.outcome.amount }} {{ budgets.monthly.outcome.currency }}</li>
-                            <li>balance: {{ budgets.monthly.balance.amount }} {{ budgets.monthly.balance.currency }}</li>
+                            <li>income:
+                                <MoneyFormatter
+                                    :value="budgets.monthly.income.amount"
+                                    :currency="budgets.monthly.income.currency"
+                                />
+                            </li>
+                            <li>outcome:
+                                <MoneyFormatter
+                                    :value="budgets.monthly.outcome.amount"
+                                    :currency="budgets.monthly.outcome.currency"
+                                />
+                            </li>
+                            <li>balance:
+                                <MoneyFormatter
+                                    :value="budgets.monthly.balance.amount"
+                                    :currency="budgets.monthly.balance.currency"
+                                />
+                            </li>
                         </ul>
                     </div>
                     <div class="p-6 text-gray-900">
                         <span>WIP</span>
                         <h3 class="text-lg font-semibold pb-4">Budget(total): </h3>
                         <ul>
-                            <li>allowed balance: {{ budgets.total.income.amount }} {{ budgets.total.income.currency }}</li>
-                            <li>current debt: {{ budgets.total.outcome.amount }} {{ budgets.total.outcome.currency }}</li>
-                            <li>balance: {{ budgets.total.balance.amount }} {{ budgets.total.balance.currency }}</li>
+                            <li>Allowed balance:
+                                <MoneyFormatter
+                                    :value="budgets.total.income.amount"
+                                    :currency="budgets.total.income.currency"
+                                />
+                            </li>
+                            <li>Current debt:
+                                <MoneyFormatter
+                                    :value="budgets.total.outcome.amount"
+                                    :currency="budgets.total.outcome.currency"
+                                />
+                            </li>
+                            <li>Balance:
+                                <MoneyFormatter
+                                    :value="budgets.total.balance.amount"
+                                    :currency="budgets.total.balance.currency"
+                                />
+                            </li>
                         </ul>
                     </div>
                 </div>
